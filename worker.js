07778121b51623f3bc811f7fc497c4ad29895f8c
fqzlr <a10275974@outlook.com>
2026-05-08 20:03:35 +0800

    fix worker config

--- worker.js
+++ worker.js
@@ -1,9 +1,5 @@
-import manifest from "__STATIC_CONTENT_MANIFEST"
-import { serveStatic } from "cloudflare-workers/serve-static"
-
-const handler = async (request) => {
-  const response = await serveStatic({ request, manifest })
-  return response || new Response("Not found", { status: 404 })
-}
-
-export default { fetch: handler }
+export default {
+  async fetch(request, env) {
+    return await env.ASSETS.fetch(request);
+  }
+};
